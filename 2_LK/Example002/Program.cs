cde4282c4801330be20f7caa667a4167644ed398
elena <lena.kazakova99@gmail.com>
2022-08-15 22:55:52 +0400

    Example2

---
+++ 2_LK/Example002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Нахождение максимума из 9 чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+//               0  1  2  3  4  5  6  7  8
+int [] array = {11,21,31,41,15,61,17,18,10};
+array[0] = 12;
+Console.WriteLine(array[4]);
+
+
+int max = Max(
+    Max(array[0], array[1], array[3]),
+    Max(array[4], array[5], array[6]),
+    Max(array[7], array[8], array[9])
+);
+Console.WriteLine(max);
